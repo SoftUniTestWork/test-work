aee7dd09b646321993d1d0318378bafc746d8106
Kati Milcheva <kati_mil4eva@abv.bg>
2017-05-30 19:09:56 +0300

    exercise

---
+++ SimpleTriangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 0; n < 10; n++)
+            {
+                for (int a = 0; a <= n; a++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
